5cd9314fd82a091d42b7d077286cb55f8c7e2953
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-27 18:46:56 +0530

    Added:Functionality to create addressbook

---
+++ AddressBook.js
@@ -0,0 +1,53 @@
+class Contact {
+    //properties
+    FirstName;
+    LastName;
+    Address;
+    City;
+    State;
+    Zip;
+    PhoneNumber;
+    Email;
+
+    //constructor with undefiend number of parameters
+    constructor(...params) {
+        this.FirstName = params[0];
+        this.LastName = params[1];
+        this.Address = params[2];
+        this.City = params[3];
+        this.State = params[4];
+        this.Zip = params[5];
+        this.PhoneNumber = params[6];
+        this.Email = params[7];
+    }
+
+    get FirstName() { return this._FirstName; }
+    set(FirstName) { this._Firstname = FirstName; }
+
+    get LastName() { return this._LastName; }
+    set(LastName) { this._Lastname = LastName; }
+
+    get Address() { return this._Address; }
+    set(Address) { this._Address = Address; }
+
+    get City() { return this._City; }
+    set(City) { this._City = City; }
+
+    get State() { return this._State; }
+    set(State) { this._State = State; }
+
+    get Zip() { return this._Zip; }
+    set(Zip) { this._Zip = Zip; }
+
+    get PhoneNumber() { return this._PhoneNumber; }
+    set(PhoneNumber) { this._PhoneNumber = PhoneNumber; }
+
+    get Email() { return this._Email; }
+    set(Email) { this.Email = Email; }
+
+    toString() {
+        return "First Name =" + this.FirstName + ", Last Name = " + this.LastName + ",Address = " + this.Address + ",City = " + this.City + ",State = " + this.State + ",Zip = " + this.Zip + ",PhoneNumber = " + this.PhoneNumber + ",Email = " + this.Email;
+    }
+}
+let personContact = new Contact("kalyan", "tanniru", "12-1234/1", "Guntur", "AndhraPradesh", "522616", "9182169100", "tannirukalyan1432@gmail.com");
+console.log(personContact.toString());
